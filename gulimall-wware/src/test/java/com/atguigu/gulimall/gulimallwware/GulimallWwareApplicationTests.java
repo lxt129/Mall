355fa0168c30ae06b6989a29cfc308a7f5d3ffd6
lxt <1293707028@qq.com>
2021-07-16 20:35:30 +0800

    项目初始结构创建

---
+++ gulimall-wware/src/test/java/com/atguigu/gulimall/gulimallwware/GulimallWwareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atguigu.gulimall.gulimallwware;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallWwareApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
